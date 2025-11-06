b1e94420a8e44b8658d2a12d994355ea89eb6d07
Theo Kramer <theokramer@MacBook-Pro-von-Theo.local>
2025-11-06 15:48:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# create_participant
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/create_participant/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.create_participant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
